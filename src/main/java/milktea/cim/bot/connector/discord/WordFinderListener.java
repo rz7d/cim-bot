3d7f6deb16fe40c92ea98f47a5a950914a436874
azure <stillcrystal@outlook.jp>
2019-03-16 04:11:23 +0900

    Separate WordFinderListener to listener and finder core

--- src/main/java/milktea/cim/bot/connector/discord/WordFinderListener.java
+++ src/main/java/milktea/cim/bot/connector/discord/WordFinderListener.java
@@ -2,57 +2,32 @@ package milktea.cim.bot.connector.discord;
 
 import java.text.MessageFormat;
 import java.text.Normalizer;
-import java.util.Collection;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
+import milktea.cim.bot.reader.WordFinder;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class WordFinderListener extends ListenerAdapter {
 
-  private static final String PATTERN_HMAGE_EN = "[HEhe]\\s*[MGmg]\\s*a\\s*[GMgm]\\s*[EHeh]";
-  private static final String PATTERN_HMAGE_JP = "[はハ]\\s*([るル])?\\s*[まマ]\\s*[げゲ]\\s*([んン]|[どド]\\s*[んン])?|春\\s*[馬真間]\\s*源";
-  private static final String PATTERN_KIZOKU_JP = "本\\s*物\\s*貴\\s*族|((半|[ハは]\\s*[ンん])\\s*(間|[マま]))\\s*(巌|[イい]\\s*[ワわ]\\s*[オお])?";
-  private static final String PATTERN_ASAYAMA_JP = "(朝|[あア]\\s*[さサ])\\s*(山|[やヤ]\\s*[まマ])\\s*(貴|[たタ]\\s*[かカ])\\s*(生|[おオ])";
-  private static final String PATTERN_YAMADA_JP = "(山|[やヤ]\\s*[まマ])\\s*(田|[だダ])\\s*(孝|[たタ]\\s*[かカ])\\s*(之|[ゆユ]\\s*[きキ])";
-  private static final String PATTERN_HOIKORO_JP = "回\\s*鍋\\s*肉";
-
-  private static final Collection<Pattern> PATTERNS = Stream
-    .of(PATTERN_HMAGE_EN,
-      PATTERN_HMAGE_JP,
-      PATTERN_KIZOKU_JP,
-      PATTERN_ASAYAMA_JP,
-      PATTERN_YAMADA_JP,
-      PATTERN_HOIKORO_JP)
-    .parallel()
-    .map(Pattern::compile)
-    .collect(Collectors.toList());
-
   @Override
   public void onMessageReceived(MessageReceivedEvent event) {
     if (Objects.equals(event.getAuthor(), event.getJDA().getSelfUser()))
       return;
 
-    for (Pattern p : PATTERNS) {
-      if (sendIfFound(event, p))
-        break;
-    }
-  }
-
-  private static boolean sendIfFound(MessageReceivedEvent event, Pattern pattern) {
     var text = event.getMessage().getContentDisplay();
-    Matcher matcher = pattern.matcher(Normalizer.normalize(text, Normalizer.Form.NFKC));
-    if (!matcher.find()) {
-      return false;
-    }
-    var found = matcher.group();
+    Optional<? extends Matcher> result = WordFinder.matcher(Normalizer.normalize(text, Normalizer.Form.NFKC));
+    if (result.isEmpty())
+      return;
 
+    Matcher matcher = result.get();
+    assert matcher.find();
+
+    var found = matcher.group();
     var name = event.getAuthor().getName();
     var avatar = event.getAuthor().getAvatarUrl();
 
@@ -62,7 +37,6 @@ public class WordFinderListener extends ListenerAdapter {
     insertInformation(text, found, matcher.start(), builder);
     builder.setFooter(name, avatar);
     event.getChannel().sendMessage(builder.build()).queue();
-    return true;
   }
 
   private static void insertInformation(String text, String found, int from, EmbedBuilder builder) {
