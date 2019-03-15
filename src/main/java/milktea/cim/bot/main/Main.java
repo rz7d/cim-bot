75de414ce85c506ddc5ad0638b3729a38cc378ad
azure <stillcrystal@outlook.jp>
2019-03-16 04:16:25 +0900

    Add security file examples

--- src/main/java/milktea/cim/bot/main/Main.java
+++ src/main/java/milktea/cim/bot/main/Main.java
@@ -12,7 +12,7 @@ public final class Main {
 
   public static void main(String[] args) throws Exception {
     var properties = new Properties();
-    properties.load(DiscordBot.class.getResourceAsStream("/secure.properties"));
+    properties.load(DiscordBot.class.getResourceAsStream("/access.properties"));
     var token = properties.getProperty("token_secret");
 
     var bot = new DiscordBot(token);
