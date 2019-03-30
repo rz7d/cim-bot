d7740fb6e86890826a19a1c75352576348ea0045
azure <rz7@outlook.jp>
2019-03-30 16:12:58 +0900

    Add FunCommandTest

---
+++ src/test/java/milktea/cim/bot/command/fun/FunCommandTest.java
@@ -0,0 +1,38 @@
+package milktea.cim.bot.command.fun;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.entities.SelfUser;
+import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+public class FunCommandTest {
+
+  private final FunnyMessages messages = new FunnyMessages();
+
+  @Test
+  public void testConstructor() {
+    assertDoesNotThrow(() -> new FunCommand(messages));
+  }
+
+  @Test
+  public void testCreateMessage() {
+    var jda = mock(JDA.class);
+    when(jda.getSelfUser()).thenReturn(mock(SelfUser.class));
+
+    var event = mock(MessageReceivedEvent.class);
+    when(event.getJDA()).thenReturn(jda);
+
+    var author = mock(User.class);
+    when(event.getAuthor()).thenReturn(author);
+
+    var o = new FunCommand(messages);
+    var result = o.createMessage(event);
+    assertNotNull(result);
+  }
+
+}
