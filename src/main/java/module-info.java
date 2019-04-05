ace28b818cd5dc1547167e9ab935155b62fd6dbc
azure <rz7@outlook.jp>
2019-04-05 19:15:57 +0900

    Delete module-info.java / Update compiler args

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-open module milktea.cim.bot {
-    requires java.desktop;
-    requires milktea.cim.base;
-    requires JDA;
-    requires java.sql;
-    requires fastjson;
-    requires slf4j.api;
-}
